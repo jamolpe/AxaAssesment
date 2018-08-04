46bee195f3ef50895ef9a548f917477fbb45ecc5
Unknown <jamolpeeps@gmail.com>
2018-08-04 02:45:03 +0200

    Proyect Base

---
+++ AxaAssesment/AxaAssesment.Library/Domain/Models/Settings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AxaAssesment.Library.Domain.Models
+{
+    class Settings
+    {
+        public string ClientsUrl { get; set; }
+        public string PoliciesUrl { get; set; }
+    }
+}
---
+++ AxaAssesment/AxaAssesment.Library/Domain/Repository/ClientRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AxaAssesment.Library.Domain.Models;
+
+namespace AxaAssesment.Library.Domain.Repository
+{
+    class ClientRepository
+    {
+        private string _serviceDataUrl;
+        public ClientRepository(Settings settings)
+        {
+            this._serviceDataUrl = settings.ClientsUrl;
+        }
+    }
+}
